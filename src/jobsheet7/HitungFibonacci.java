69889a861a3e052d30147209d2f3d45f595e8be3
ASUS <ASUS@LAPTOP-929I2GH2>
2018-09-13 13:00:25 +0700

    Tambah Tampil Hasil

---
+++ src/jobsheet7/HitungFibonacci.java
@@ -0,0 +1,15 @@
+package jobsheet7;
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : "+ identitas);
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ...dst\n");
+    }
+    public static void main(String[] args){
+     String identitas = "Amalina Zakiyah Salsabila Aribowo / XR2 / 04";
+    tampilJudul(identitas);
+    
+    }
+
+}
