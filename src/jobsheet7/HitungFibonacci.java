2b245a0d8c9651fccc2647f4a27b7f0b8a7040da
ASUS <ASUS@LAPTOP-929I2GH2>
2018-09-13 13:26:09 +0700

    Tambah Tampil Hasil

--- src/jobsheet7/HitungFibonacci.java
+++ src/jobsheet7/HitungFibonacci.java
@@ -14,7 +14,7 @@ public class HitungFibonacci {
     tampilJudul(identitas);
     int n = tampilInput();
     BigInteger hasil = fibo(n);
-    
+    tampilHasil(n, hasil);
     }
     private static int tampilInput(){
         Scanner scanner = new Scanner (System.in);
@@ -35,6 +35,10 @@ public class HitungFibonacci {
             hasil[i] = hasil[i-1].add(hasil[i-2]);
         }
         return hasil[n-1];
+    }
+    private static void tampilHasil(int n, BigInteger hasil)
+    {
+        System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
     }
 
 }
