7f59caff4d9fbbe12927dcacd5eb58b7d2a064a7
ASUS <ASUS@LAPTOP-929I2GH2>
2018-09-13 13:20:13 +0700

    Tambah Hitung Fibonacci

--- src/jobsheet7/HitungFibonacci.java
+++ src/jobsheet7/HitungFibonacci.java
@@ -1,5 +1,5 @@
 package jobsheet7;
-
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class HitungFibonacci {
@@ -13,6 +13,8 @@ public class HitungFibonacci {
      String identitas = "Amalina Zakiyah Salsabila Aribowo / XR2 / 04";
     tampilJudul(identitas);
     int n = tampilInput();
+    BigInteger hasil = fibo(n);
+    
     }
     private static int tampilInput(){
         Scanner scanner = new Scanner (System.in);
@@ -21,6 +23,18 @@ public class HitungFibonacci {
         int n = scanner.nextInt();
         return n;
         
+    }
+    private static BigInteger fibo(int n) {
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++)
+        {
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        return hasil[n-1];
     }
 
 }
